67826212622ad3a92f5181c9398bdc51d7b92385
kovidbehl97 <58339070+kovidbehl97@users.noreply.github.com>
2020-06-08 15:08:38 +0530

    Add files via upload
    home academy us

---
+++ HAUS/resources/js/index.js
@@ -0,0 +1,125 @@
+function firstGrade(){
+    let grade1 = document.getElementById("grade-1-content")
+    if (grade1.style.display === "block"){
+        grade1.style.display = "none"
+    }else{
+        grade1.style.display = "block"
+    }     
+}
+function secondGrade(){
+    let grade2 = document.getElementById("grade-2-content")
+    if (grade2.style.display === "block"){
+        grade2.style.display = "none"
+    }else{
+        grade2.style.display = "block"
+    }     
+}
+function thirdGrade(){
+    let grade3 = document.getElementById("grade-3-content")
+    if (grade3.style.display === "block"){
+        grade3.style.display = "none"
+    }else{
+        grade3.style.display = "block"
+    }     
+}
+function fourthGrade(){
+    let grade4 = document.getElementById("grade-4-content")
+    if (grade4.style.display === "block"){
+        grade4.style.display = "none"
+    }else{
+        grade4.style.display = "block"
+    }     
+}
+function fifthGrade(){
+    let grade5 = document.getElementById("grade-5-content")
+    if (grade5.style.display === "block"){
+        grade5.style.display = "none"
+    }else{
+        grade5.style.display = "block"
+    }     
+}
+function firsthGradeNav(){
+    let grade1 = document.getElementById("grade-1-content")
+    if (grade1.style.display === "none"){
+        grade1.style.display = "block"
+    }else{
+        grade1.style.display = "block"
+    }   
+}
+function secondGradeNav(){
+    let grade2 = document.getElementById("grade-2-content")
+    if (grade2.style.display === "none"){
+        grade2.style.display = "block"
+    }else{
+        grade2.style.display = "block"
+    }    
+}
+function thirdGradeNav(){
+    let grade3 = document.getElementById("grade-3-content")
+    if (grade3.style.display === "none"){
+        grade3.style.display = "block"
+    }else{
+        grade3.style.display = "block"
+    }    
+}
+function fourthGradeNav(){
+    let grade4 = document.getElementById("grade-4-content")
+    if (grade4.style.display === "none"){
+        grade4.style.display = "block"
+    }else{
+        grade4.style.display = "block"
+    }    
+}
+function fifthGradeNav(){
+    let grade5 = document.getElementById("grade-5-content")
+    if (grade5.style.display === "none"){
+        grade5.style.display = "block"
+    }else{
+        grade5.style.display = "block"
+    }    
+}
+function sub(){
+    let subjects = document.getElementById("subjects")
+    let gradeNav = document.getElementById("gradesNav")
+    if (subjects.style.display === "block"){
+        subjects.style.display = "none"
+    }else{
+        subjects.style.display = "block"
+        gradeNav.style.display = "none"
+    }     
+}
+function gradesNav(){
+    let gradeNav = document.getElementById("gradesNav")
+    let subjects = document.getElementById("subjects")
+    if (gradeNav.style.display === "block"){
+        gradeNav.style.display = "none"
+    }else{
+        gradeNav.style.display = "block"
+        subjects.style.display = "none"
+    }     
+}
+
+function maths(){
+    let maths = document.getElementById('maths')
+    if (maths.style.display === "block"){
+        maths.style.display = "none"
+    }else{
+        maths.style.display = "block"
+    }
+}
+function science(){
+    let science = document.getElementById('science')
+    if (science.style.display === "block"){
+        science.style.display = "none"
+    }else{
+        science.style.display = "block"
+    }
+}
+function sst(){
+    let sst = document.getElementById('sst')
+    if (sst.style.display === "block"){
+        sst.style.display = "none"
+    }else{
+        sst.style.display = "block"
+    }
+}
